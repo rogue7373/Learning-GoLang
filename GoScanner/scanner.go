66ec28982aa911a9d2b8b52cd42e532113b3f779
rogue7373 <matt.lorenzen@gmail.com>
2023-08-17 10:25:50 -0600

    Update scanner.go

--- GoScanner/scanner.go
+++ GoScanner/scanner.go
@@ -10,7 +10,7 @@ func main() {
 	ports := []int{80, 443, 22, 8080}
 
 	for _, port := range ports {
-		address := fmt.Sprintf("mcdonalds.com:%d", port)
+		address := fmt.Sprintf("google.com:%d", port)
 		conn, err := net.Dial("tcp", address)
 		if err != nil {
 			if strings.Contains(err.Error(), "connection refused") {
